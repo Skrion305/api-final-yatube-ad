ef5e1e5fa1851fc03e796bb84ee46ec48555d812
Kirill Sirotkin <kirasir2006@gmail.com>
2026-08-30 19:13:19 +0300

    Update urls.py

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,8 +1,18 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from rest_framework.routers import DefaultRouter
+from posts.views import PostViewSet, CommentViewSet, GroupViewSet, FollowViewSet
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet, basename='post')
+router.register(r'groups', GroupViewSet, basename='group')
+router.register(r'follow', FollowViewSet, basename='follow')
 
 urlpatterns = [
     path('api/v1/jwt/create/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/jwt/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/v1/jwt/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('', include(router.urls)),
+    path('posts/<int:post_id>/comments/', CommentViewSet.as_view({'get': 'list', 'post': 'create'}), name='comment-list'),
+    path('posts/<int:post_id>/comments/<int:pk>/', CommentViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'}), name='comment-detail'),
 ]
